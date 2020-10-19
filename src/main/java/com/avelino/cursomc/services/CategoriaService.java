4895d30d7e9df26f7f877ec027dd9dabc15c5f5a
Carlos Avelino <carlos.avelino2.0@gmail.com>
2020-10-19 11:51:25 -0300

    Ajustes no endpoint / categorias/{id}

--- src/main/java/com/avelino/cursomc/services/CategoriaService.java
+++ src/main/java/com/avelino/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.avelino.cursomc.domain.Categoria;
 import com.avelino.cursomc.repositories.CategoriaRepository;
+import com.avelino.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objecto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 }
