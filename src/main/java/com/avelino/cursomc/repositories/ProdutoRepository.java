344827c1dd0882bab1f5dcfa25161b4d2d657c74
Carlos Avelino <carlos.avelino2.0@gmail.com>
2020-10-18 21:26:16 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/avelino/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/avelino/cursomc/repositories/ProdutoRepository.java
@@ -3,10 +3,11 @@ package com.avelino.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.avelino.cursomc.domain.Categoria;
+import com.avelino.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	
 	
 }
--- src/main/java/com/avelino/cursomc/services/CategoriaService.java
+++ src/main/java/com/avelino/cursomc/services/CategoriaService.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.avelino.cursomc.domain.Categoria;
-import com.avelino.cursomc.repositories.categoriaRepository;
+import com.avelino.cursomc.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaService {
 	
 	@Autowired
-	private categoriaRepository repo;
+	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
