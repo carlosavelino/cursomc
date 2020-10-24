ed914f3582ee6d16459bd2a41f7c3d37eb1b365c
Carlos Avelino <carlos.avelino2.0@gmail.com>
2020-10-24 17:28:22 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/avelino/cursomc/domain/Pagamento.java
+++ src/main/java/com/avelino/cursomc/domain/Pagamento.java
@@ -10,9 +10,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-
 import com.avelino.cursomc.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
@@ -23,7 +22,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
